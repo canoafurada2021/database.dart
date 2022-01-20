27ca3396c4f79397226fa47ac4030f989e3f5f93
Canoa furada =) <88066241+canoafurada2021@users.noreply.github.com>
2022-01-20 18:52:58 -0300

    Update app_database.dart

--- app_database.dart
+++ app_database.dart
@@ -1,44 +1,42 @@
-//creating database and data table on version number one
-
-
+import 'dart:async';
 import 'package:path/path.dart';
-import 'pckage:sqflite/sqflite.dart'
+import 'package:sqflite/sqflite.dart';
+
+import '../models/contact.dart';
 
 Future<Database> createDatabase(){
- return getDatabasesPath().then((dbPath){
-  final String path = join(dbPath. 'bytebank.db');
-   return openDatabase(path, onCreate: (db,version) {
-db.execute('CREATE TABLE contacts('
-     'id INTEGER PRIMARY KEY, '
-      'account_number INTEGER)');
-}, version: 1);
-});
-}
- //importe a class contact
-Future <int>  save(contact contact){
- return  createDatabase().then((db){
-  final Map<String, dynamic> contactMap = Map();
-            // contactMap['id'] = contact.id; removido pois Sqlite incrementa id's do tipo inteiro
-   contactMap['name'] = contact.name;
-   contactMap['account_number'] = contact.accountNumber;
-  rturn db.insert('contacts', contactMap); //uso de future para execução assíncrona
-  });
- }
- //db.query pega todos os dados de uma tabela
-Future <List<Contact>> findAll(){ //retornando then  contato e then do querry
- return  createDatabase().then((db){
-return  db.query('contacts').then((maps){
-  final List<Contact> contacts = List [];
-  for(Map <String, dynamic> map in maps){
-   final Contact contac = Contact( //uma instancia de um contato
-    map['id'], //sempre o  mesmo nome inserido na tabela Datatbase
-    map['name'],
-    map['account_number'], 
+  return getDatabasesPath().then((dbPath) {
+    final String path = join(dbPath,"bytebank.db");
+    return openDatabase(path, onCreate: (db,version) {
+      db.execute('CREATE TABLE contacts('
+          'id INTEGER PRIMARY KEY, '
+          'name TEXT,'
+          'account_number INTEGER)');
+    }, version: 2,
+    onDowngrade: onDatabaseDowngradeDelete
     );
- contacts.add(contact); //lista preenchida com todos os dados buscados na tabela de contatos
-  }
-  return contacts;
-  });
   });
 }
-   }
+
+Future <int> save(Contact contact){
+  return  createDatabase().then((db){
+    final Map<String, dynamic> contactMap = {};
+    contactMap['name'] = contact.name;
+    contactMap['account_number'] = contact.accountNumber;
+    return db.insert('contacts', contactMap); //uso de future para execução assíncrona
+  });
+}
+
+
+Future<List<Contact>> findAll() {
+  return createDatabase().then((db) {
+    return db.query('contacts').then((maps) {
+      final List<Contact> contacts = List.empty();
+      for (Map<String, dynamic> map in maps) {
+        final Contact contact = Contact(map["id"], map["name"], map["account_number"]);
+        contacts.add(contact);
+      }
+      return contacts;
+    });
+  });
+}
