94b5c3109c3eb9696632e59c07f69eb220eb5365
Canoa furada =) <88066241+canoafurada2021@users.noreply.github.com>
2022-01-19 23:26:04 -0300

    Create contact.dart

---
+++ contact.dart
@@ -0,0 +1,15 @@
+class contact{
+  final int id;
+  final string name;
+  final int accountNumber;
+  
+  Contact(
+    this.id, //para ter acesso a class id dentro do database
+    this.name,
+    this.accountNumber,
+    );
+  
+  @override
+  String toString(){
+    return 'Contact{name: $name, accountNumber: $accountNumber};
+    }
