8b1f03bffe4280fe56e266d9dcf108c0a004a7c3
Canoa furada =) <88066241+canoafurada2021@users.noreply.github.com>
2022-01-20 00:04:13 -0300

    Update app_database.dart

--- app_database.dart
+++ app_database.dart
@@ -15,11 +15,30 @@ db.execute('CREATE TABLE contacts('
 });
 }
  //importe a class contact
- void save(contact contact){
-  createDatabase().then((db){
+Future <int>  save(contact contact){
+ return  createDatabase().then((db){
   final Map<String, dynamic> contactMap = Map();
-  contactMap['id'] = contact.id
-  db.insert('contacts', values)
+  contactMap['id'] = contact.id;
+   contactMap['name'] = contact.name;
+   contactMap['account_number'] = contact.accountNumber;
+  rturn db.insert('contacts', contactMap); //uso de future para execução assíncrona
   });
  }
- 
+ //db.query pega todos os dados de uma tabela
+Future <List<Contact>> findAll(){ //retornando then  contato e then do querry
+ return  createDatabase().then((db){
+return  db.query('contacts').then((maps){
+  final List<Contact> contacts = List [];
+  for(Map <String, dynamic> map in maps){
+   final Contact contac = Contact( //uma instancia de um contato
+    map['id'], //sempre o  mesmo nome inserido na tabela Datatbase
+    map['name'],
+    map['account_number'], 
+    );
+ contacts.add(contact); //lista preenchida com todos os dados buscados na tabela de contatos
+  }
+  return contacts;
+  });
+  });
+}
+   }
