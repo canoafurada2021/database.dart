87abec282ac3143697ffa38627808ea57cfd39f0
Canoa furada =) <88066241+canoafurada2021@users.noreply.github.com>
2022-01-20 18:54:25 -0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,8 +1,28 @@
-void main() { //transofrmando o void em bloco e inserindo as funçoes criadas no database
-  runApp(BytebankApp());
-  save(Contact(0, 'Bruna  ,1000)).then((id){
-  findAll().then ((contacts) => debugPrint(contacts.toString()))); //fazendo a busca dentro do database
-  });
+import 'package:bytebankapp/database/app_database.dart';
+import 'package:bytebankapp/Screens/contact_form.dart';
+import 'package:bytebankapp/Screens/dashboard.dart';
+import 'package:flutter/material.dart';
+import 'models/contact.dart';
+
+void main(){
+
+runApp(BytebankApp());
+findAll().then((contacts) => debugPrint(contacts.toString()));
+}
+ class BytebankApp extends StatelessWidget {
+  const BytebankApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(
+            primaryColor: Colors.green[900],
+            colorScheme: ColorScheme.fromSwatch()
+                .copyWith(secondary: Colors.blueAccent[700]),
+            buttonTheme: ButtonThemeData(
+              buttonColor: Colors.blueAccent[700],
+              textTheme: ButtonTextTheme.primary,
+            )),
+        home: const Dashboard());
   }
-    [...]
-    //no console deverão aparecer as informações como [Contact{name: bruna, accountNumber: 1000}]
+}
