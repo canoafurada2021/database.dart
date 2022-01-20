2c831c35ec2ae52eb17bc8123cce5c3a6fb520d4
Canoa furada =) <88066241+canoafurada2021@users.noreply.github.com>
2022-01-20 18:52:26 -0300

    Update contact.dart

--- contact.dart
+++ contact.dart
@@ -1,15 +1,18 @@
-class contact{
-  final int id;
-  final string name;
-  final int accountNumber;
-  
-  Contact(
-    this.id, //para ter acesso a class id dentro do database
+class Contact
+{
+final int id;
+final String name;
+final int? accountNumber;
+
+
+Contact(
+    this.id,
     this.name,
-    this.accountNumber,
-    );
-  
-  @override
-  String toString(){
-    return 'Contact{id: $id, name: $name, accountNumber: $accountNumber};
-    }
+    this.accountNumber);
+
+@override
+String toString() {
+  return 'Contact{id: $id, name: $name, accountNumber: $accountNumber}';
+}
+
+}
