acb5fe7c6801e39417735a8fc18af443b7a66889
Canoa furada =) <88066241+canoafurada2021@users.noreply.github.com>
2022-01-20 18:53:18 -0300

    Update contac_form.dart

--- contac_form.dart
+++ contac_form.dart
@@ -1,7 +1,70 @@
-[...]
- child: text('Create'),
-onPressed () {
-  final string name= _nameController.text;
-  final int? accountnumber = int.tryParse(_accountNumberControler.text):
-  final Contact newContact = Contact(0,name,accountNumber);
-  Navigator.pop(context, nemContext);
+import 'package:bytebankapp/models/contact.dart';
+import 'package:flutter/material.dart';
+
+import '../database/app_database.dart';
+
+class ContactForm extends StatefulWidget {
+  const ContactForm({Key? key}) : super(key: key);
+
+  @override
+  _ContactFormState createState() => _ContactFormState();
+}
+
+class _ContactFormState extends State<ContactForm> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _accountNumberController =
+      TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green[900],
+          title: const Text('New Contact'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              TextField(
+                controller: _nameController,
+                decoration: const InputDecoration(
+                  labelText: 'Full Name',
+                ),
+                style: const TextStyle(
+                  fontSize: 24.0,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: TextField(
+                  controller: _accountNumberController,
+                  decoration: const InputDecoration(
+                    labelText: 'Account Number',
+                  ),
+                  style: const TextStyle(
+                    fontSize: 24.0,
+                  ),
+                  keyboardType: TextInputType.number,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: SizedBox(
+                    width: double.maxFinite,
+                    child: ElevatedButton(
+                      child: const Text("Create"),
+                      onPressed: () {
+                        final String name = _nameController.text;
+                        final int? accountNumber =
+                            int.tryParse(_accountNumberController.text);
+                        final Contact newContact = Contact(0,name,accountNumber);
+                        save(newContact).then((id) =>Navigator.pop(context, newContact));
+                      },
+                    )),
+              )
+            ],
+          ),
+        ));
+  }
+}
