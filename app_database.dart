ecd3182179f71f37ff1180d1fa8855cfc0b67ed3
Canoa furada =) <88066241+canoafurada2021@users.noreply.github.com>
2022-01-21 17:45:36 -0300

    Update app_database.dart

--- app_database.dart
+++ app_database.dart
@@ -1,42 +1,44 @@
 import 'dart:async';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
-
 import '../models/contact.dart';
 
-Future<Database> createDatabase(){
-  return getDatabasesPath().then((dbPath) {
-    final String path = join(dbPath,"bytebank.db");
-    return openDatabase(path, onCreate: (db,version) {
+Future<int> save(Contact contact) async {
+  final Database db = await createDatabase();
+  final Map<String, dynamic> contactMap = {};
+  contactMap['name'] = contact.name;
+  contactMap['account_number'] = contact.accountNumber;
+  return db.insert('contacts', contactMap);
+}
+
+
+
+  Future<List<Contact>> findAll() async {
+    final Database db = await createDatabase();
+    final List<Map<String, dynamic>> result = await db.query('contacts');
+    final List<Contact> contacts = [];
+    for (Map<String, dynamic> row in result) {
+      final Contact contact = Contact(
+        row["id"],
+        row["name"],
+        row["account_number"],
+      );
+      contacts.add(contact);
+    }
+    return contacts;
+  }
+
+Future<Database> createDatabase() async {
+  final String path = join(await getDatabasesPath(), 'bytebank.db');
+  return openDatabase(
+    path,
+    onCreate: (db, version) {
       db.execute('CREATE TABLE contacts('
           'id INTEGER PRIMARY KEY, '
           'name TEXT,'
           'account_number INTEGER)');
-    }, version: 2,
-    onDowngrade: onDatabaseDowngradeDelete
-    );
-  });
-}
+    },
+    version: 1,
+    onDowngrade: onDatabaseDowngradeDelete,
+  );
 
-Future <int> save(Contact contact){
-  return  createDatabase().then((db){
-    final Map<String, dynamic> contactMap = {};
-    contactMap['name'] = contact.name;
-    contactMap['account_number'] = contact.accountNumber;
-    return db.insert('contacts', contactMap); //uso de future para execução assíncrona
-  });
-}
-
-
-Future<List<Contact>> findAll() {
-  return createDatabase().then((db) {
-    return db.query('contacts').then((maps) {
-      final List<Contact> contacts = List.empty();
-      for (Map<String, dynamic> map in maps) {
-        final Contact contact = Contact(map["id"], map["name"], map["account_number"]);
-        contacts.add(contact);
-      }
-      return contacts;
-    });
-  });
-}
