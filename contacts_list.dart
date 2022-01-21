95ab5732c7a5ec70f9b6edec0bd0f6e554d2aeae
Canoa furada =) <88066241+canoafurada2021@users.noreply.github.com>
2022-01-21 17:46:47 -0300

    Update contacts_list.dart

--- contacts_list.dart
+++ contacts_list.dart
@@ -1,78 +1,77 @@
-import 'package:bytebankapp/Screens/contact_form.dart';
-import 'package:bytebankapp/database/app_database.dart';
-import 'package:flutter/material.dart';
+import 'package:bytebankapp/database/dao/contact_dao.dart';
 import 'package:bytebankapp/models/contact.dart';
+import 'package:bytebankapp/screens/contact_form.dart';
+import 'package:flutter/material.dart';
+
 
 class ContactsList extends StatelessWidget {
   const ContactsList({Key? key}) : super(key: key);
+
+
+
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-        title: const Text('Contacts'), backgroundColor: Colors.green[900]),
+          title: Text('Contacts'),
+        ),
+        body: FutureBuilder<List<Contact>>(
+        initialData: const [],
+          future: findAll(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+                break;
+              case ConnectionState.waiting:
+                return Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: const <Widget>[
+                      CircularProgressIndicator(),
+                      Text('Loading')
+                    ],
+                  ),
+                );
 
-    body: FutureBuilder<List<Contact>>(
-    initialData: List.empty(),
-    future: findAll(),
-    builder: (context, snapshot) {
-    switch (snapshot.connectionState) {
-    case ConnectionState.none:
-    break;
-    case ConnectionState.waiting:
-    return Center(
-    child: Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    crossAxisAlignment: CrossAxisAlignment.center,
-    children: const <Widget>[
-    CircularProgressIndicator(),
-    Text('Loading')
-    ],
-    ),
-    );
-
-    case ConnectionState.active:
-    break;
-    case ConnectionState.done:
-    final List<Contact> contacts = snapshot.data as List<Contact>;
-    return ListView.builder(
-    itemBuilder: (context, index) {
-    final Contact contact = contacts[index];
-    return _ContactItem(contact);
-    },
-    itemCount: contacts.length,
-    );
-    }
-
-    return const Text("Unknown error");
-    },
-    ),
-    //
-    floatingActionButton: FloatingActionButton(
-    onPressed: () {
-    Navigator.of(context).push(
-    MaterialPageRoute(
-    builder: (context) => const ContactForm(),
-    ),
-    );
-    },
-    child: const Icon(
-    Icons.add),
-    ),
+              case ConnectionState.active:
+                break;
+              case ConnectionState.done:
+                final List<Contact>? contacts = snapshot.data;
+                return ListView.builder(
+                  itemBuilder: (context, index) {
+                    final Contact contact = contacts![index];
+                    return _ContactItem(contact);
+                  },
+                  itemCount: contacts?.length,
+                );
+                break;
+            }
+            return const Text("Unknown Error");
+          },
+        ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => const ContactForm(),
+            ),
+          );
+        },
+        child: const Icon(
+          Icons.add,
+        ),
+      ),
     );
   }
-
-
 }
-
 class _ContactItem extends StatelessWidget {
   final Contact contact;
-
   const _ContactItem(this.contact);
-
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Card(
       child: ListTile(
         title: Text(
@@ -83,10 +82,12 @@ class _ContactItem extends StatelessWidget {
         ),
         subtitle: Text(
           contact.accountNumber.toString(),
-          maxLines: 2,
-          style: const TextStyle(fontSize: 16.0),
+          style: const TextStyle(
+            fontSize: 16.0,
+          ),
         ),
       ),
     );
   }
+
 }
