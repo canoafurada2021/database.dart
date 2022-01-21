af391141c226547f666ca60b93fe9e4706b7398d
Canoa furada =) <88066241+canoafurada2021@users.noreply.github.com>
2022-01-21 17:46:05 -0300

    Update contac_form.dart

--- contac_form.dart
+++ contac_form.dart
@@ -1,7 +1,6 @@
 import 'package:bytebankapp/models/contact.dart';
 import 'package:flutter/material.dart';
-
-import '../database/app_database.dart';
+import 'package:bytebankapp/database/app_database.dart';
 
 class ContactForm extends StatefulWidget {
   const ContactForm({Key? key}) : super(key: key);
@@ -13,7 +12,9 @@ class ContactForm extends StatefulWidget {
 class _ContactFormState extends State<ContactForm> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _accountNumberController =
-      TextEditingController();
+  TextEditingController();
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -57,9 +58,9 @@ class _ContactFormState extends State<ContactForm> {
                       onPressed: () {
                         final String name = _nameController.text;
                         final int? accountNumber =
-                            int.tryParse(_accountNumberController.text);
-                        final Contact newContact = Contact(0,name,accountNumber);
-                        save(newContact).then((id) =>Navigator.pop(context, newContact));
+                        int.tryParse(_accountNumberController.text);
+                        final Contact newContact = Contact(0, name, accountNumber);
+                         save(newContact).then((id) => Navigator.pop(context));
                       },
                     )),
               )
