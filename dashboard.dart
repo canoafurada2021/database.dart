567254bab35514e0edd8e945eae1d950da546e7d
Canoa furada =) <88066241+canoafurada2021@users.noreply.github.com>
2022-01-24 18:57:49 -0300

    Update dashboard.dart

--- dashboard.dart
+++ dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:bytebankapp/Screens/contacts_list.dart';
+import 'package:bytebankapp/Screens/transactions_list.dart';
 import 'package:flutter/material.dart';
 
 class Dashboard extends StatelessWidget {
@@ -12,52 +13,94 @@ class Dashboard extends StatelessWidget {
         backgroundColor: Colors.green[900],
       ),
       body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/bytebank_logo.png'),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Material(
-                color: Theme
-                    .of(context)
-                    .primaryColor,
-                child: InkWell(
-                  onTap: () {
-                    Navigator
-                        .of(context)
-                        .push(MaterialPageRoute(
-                        builder: (context) => const ContactsList()
-                    ),
-                    );
-                    },
-                  child: Container(
-                      padding: const EdgeInsets.all(8.0),
-                      height: 100,
-                      width: 150,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: const <Widget>[
-                          Icon(
-                            Icons.people,
-                            color: Colors.white,
-                            size: 32.0,
-                          ),
-                          Text("Contacts",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 16.0,
-                              )),
-                        ],
-                      )),
-                ),
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Image.asset('images/bytebank_logo.png'),
+          ),
+          Row(
+            children: <Widget>[
+              _FeatureItem(
+                'Transfer',
+                Icons.monetization_on,
+                onClick: () => _showContactsList(context),
               ),
-            )
-          ]),
+              _FeatureItem(
+                'Transaction Feed',
+                Icons.description,
+                onClick: () => _showTranscationsList(context),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showContactsList(BuildContext context) {
+    Navigator.of(context).push(
+        MaterialPageRoute(
+        builder: (context) => const ContactsList(),
+        ),
+    );
+    }
+
+  _showTranscationsList(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => TransactionsList(),
+      ),
     );
   }
 }
+
+class _FeatureItem extends StatelessWidget {
+  final String name;
+  final IconData icon;
+  final Function onClick;
+
+  const _FeatureItem(
+    this.name,
+    this.icon, {
+    required this.onClick,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Material(
+        color: Theme.of(context).primaryColor,
+        child: InkWell(
+          onTap: () => onClick(),
+          child: Container(
+            padding: const EdgeInsets.all(8.0),
+            height: 100,
+            width: 150,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Icon(
+                  icon,
+                  color: Colors.white,
+                  size: 24.0,
+                ),
+                Text(
+                  name,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.0,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
